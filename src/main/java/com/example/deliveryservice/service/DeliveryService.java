be272d45e46776caba024ecac64cb12721d77066
AigerinKazNu <talapkyzyaikerim@mail.ru>
2026-01-21 01:46:36 +0600

    webflux

--- src/main/java/com/example/deliveryservice/service/DeliveryService.java
+++ src/main/java/com/example/deliveryservice/service/DeliveryService.java
@@ -1,18 +1,26 @@
 package com.example.deliveryservice.service;
 
 import com.example.deliveryservice.dto.DeliveryRequest;
-
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 @Service
 public class DeliveryService {
 
     public Mono<String> create(DeliveryRequest request) {
+
+        System.out.println(
+                "DELIVERY SERVICE CALLED productId=" +
+                        request.getProductId() +
+                        ", address=" +
+                        request.getAddress()
+        );
+
         return Mono.just(
                 "Delivery created for productId=" +
                         request.getProductId() +
-                        ", address=" + request.getAddress()
+                        ", address=" +
+                        request.getAddress()
         );
-
-}}
+    }
+}
